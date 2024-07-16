37ab4e90abe2908104f2d95464b54188779aebb4
ShivamDeadlocker <prajapatishivam635@gmail.com>
2024-07-16 21:25:13 +0530

    EMS

---
+++ include/footer.php
@@ -0,0 +1,2 @@
+<div class="pull-right hidden-xs"> <b>Version</b> 3.3.0 </div>
+<strong>Copyright &copy; 2024 ShivamPro <a href="https://shivamdeadlocker.github.io/FirstLook/" target="_blank">Contact</a>.</strong> All rights reserved.
